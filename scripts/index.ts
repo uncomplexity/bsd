78ba58a7cc21162c5f4ce69548fdf5ee1a900907
uncomplexity <uncomplexity@uncomplexities.com>
2026-03-29 04:43:15 +0800

    save

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert";
 import fs from "node:fs";
 import path from "node:path";
 import { convert } from "@kreuzberg/html-to-markdown-wasm";
@@ -17,9 +18,11 @@ const outlines = [
 
 const browser = await chromium.launch({ headless: true });
 
-for (const outline of outlines) {
+for (let i = 0, l = outlines.length; i < l; i += 1) {
+	const outline = outlines[i];
+	assert(outline instanceof Object);
 	const outline_name = slugify(outline.name);
-  console.log(`${outline_name}..`);
+	console.log(`${outline_name}..`);
 	const page = await browser.newPage();
 	await page.goto(outline.url, { waitUntil: "networkidle" });
 	await page.evaluate(() => {
@@ -34,21 +37,31 @@ for (const outline of outlines) {
 		}));
 	});
 	await page.close();
-	const outline_path = path.join(import.meta.dirname, outline_name);
-  if (!fs.existsSync(outline_path)) {
-    fs.mkdirSync(outline_path, { recursive: true });
-  }
-	for (const subpage of subpages) {
+	const outline_path = path.join(
+		import.meta.dirname,
+		"../content/",
+		outline_name,
+	);
+	if (!fs.existsSync(outline_path)) {
+		fs.mkdirSync(outline_path, { recursive: true });
+	}
+	for (let i = 0, l = subpages.length; i < l; i += 1) {
+		const subpage = subpages[i];
+		assert(subpage instanceof Object);
 		const page_name = slugify(subpage.name);
-    console.log(`${outline_name} > ${page_name}..`);
+		console.log(`${outline_name} > ${page_name}..`);
 		const page = await browser.newPage();
 		await page.goto(subpage.url, { waitUntil: "networkidle" });
 		await page.evaluate(() => {
-			document.querySelectorAll(".headerArea, .footerArea, .menuBase, .rightButtons, #sideNav").forEach((el) => el.remove());
+			document
+				.querySelectorAll(
+					".headerArea, .footerArea, .menuBase, .rightButtons, #sideNav",
+				)
+				.forEach((el) => el.remove());
 		});
 		const page_html = await page.content();
 		const page_markdown = convert(page_html, { extractMetadata: false });
-		const page_filename = `${page_name}.md`;
+		const page_filename = `${i}-${page_name}.md`;
 		const page_filepath = path.join(outline_path, page_filename);
 		fs.writeFileSync(page_filepath, page_markdown);
 		await page.close();

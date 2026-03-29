49fbc766d56d87651630cfa331cdeca3bbf5be92
uncomplexity <uncomplexity@uncomplexities.com>
2026-03-29 09:33:17 +0800

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 import path from "node:path";
 import nextra from "nextra";
 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  }
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: "build"
+}
+
 // Set up Nextra with its configuration
 const withNextra = nextra({
 	// ... Add Nextra-specific options here
@@ -9,6 +21,9 @@ const withNextra = nextra({
 // Export the final Next.js config with Nextra included
 export default withNextra({
 	// ... Add regular Next.js options here
+
+	...nextConfig,
+
 	turbopack: {
 		root: path.join(import.meta.dirname, ".."),
 	},
